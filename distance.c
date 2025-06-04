d8375e94a70eba88e822f1ccc6a05a05c3de9aba
saquib27 <saquibshamshi27@gmail.com>
2025-06-04 21:04:43 +0530

    distance conversion

---
+++ distance.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+void main()
+
+{
+    float a;
+    printf("enter distance in KM ");
+    scanf("%f",&a);
+    float c= a*1000;
+    printf("distance in meter= %.1f\n",c);
+    
+
+
+
+}

3148dd1e05602bc319b4f79b82422daf0b70ca23
saquib27 <saquibshamshi27@gmail.com>
2025-06-04 20:33:24 +0530

    gross salary

---
+++ salary.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+void main () {
+float a;
+printf("enter your basic salary");
+
+scanf("%f",&a);
+float your_gross_salary= a+(0.4*a)+(0.2*a);
+printf("your gross salary %0.2f\n", your_gross_salary);
+
+
+
+
+}

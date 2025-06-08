032a82a37c25b587e70ffcda479278ab15626496
saquib27 <saquibshamshi27@gmail.com>
2025-06-08 18:28:31 +0530

    find youngest

--- younger.c
+++ younger.c
@@ -1,8 +1,31 @@
 #include<stdio.h>
 void main(){
-    int ram;
-    printf("ENTER RAM AGE");
-    scanf("%d\n",&ram);
+    int a,b,c;
+    printf("ENTER AGE OF a\n");
+    scanf("%d",&a);
+    printf("ENTER AGE OF b\n");
+    scanf("%d",&b);
+    printf("ENTER AGE OF c\n");
+    scanf("%d",&c);
+    if(a<b && a<c)
+    {
+        printf("a is youngest");
+    }
+
+    else if(b<a && b<c)
+    {
+        printf("b is youngest");
+    }
+    else if(c<a && c<b)
+    {
+        printf("c is youngest");
+    }
+    else 
+    {
+        printf("ages are same\n");
+    }
+
+
     
 
 

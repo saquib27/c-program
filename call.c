f9cdec9901052ae4072ef1fa0badddd2014d607b
saquib27 <saquibshamshi27@gmail.com>
2025-06-11 23:37:49 +0530

    call

---
+++ call.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+void dhoni();
+void virat();
+void main(){
+    char x;
+printf("select d for dhoni or v for virat");
+
+scanf("%c",&x);
+
+if(x=='d'){
+dhoni();
+}
+else if(x=='v'){
+    virat();
+}
+else{
+    printf("please select the correct option");
+
+}
+
+}
+
+void dhoni() {
+    printf("dhoni\n");
+}
+
+void virat(){
+    printf("virat\n");
+}

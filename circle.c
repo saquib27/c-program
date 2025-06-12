be22aec5c212812ec43718c9f22b4b06d2476f5e
saquib27 <saquibshamshi27@gmail.com>
2025-06-12 13:14:33 +0530

    circle

--- circle.c
+++ circle.c
@@ -1,13 +1,11 @@
-#include<stdio.h>
-void main(){
-float diameter_of_circle;
-printf("Enter Diameter of circle\n");
-scanf("%f",&diameter_of_circle);
-float circumference=(3.14*diameter_of_circle);
-float area=(3.14*(diameter_of_circle*diameter_of_circle)/4);
-printf("circumference of circle %.2f\n",circumference);
-printf("area of circle %.2f\n",area);
-
-
-
+#include <stdio.h>
+void main()
+{
+    float diameter_of_circle;
+    printf("Enter Diameter of circle\n");
+    scanf("%f", &diameter_of_circle);
+    float circumference = (3.14 * diameter_of_circle);
+    float area = (3.14 * (diameter_of_circle * diameter_of_circle) / 4);
+    printf("circumference of circle %.2f\n", circumference);
+    printf("area of circle %.2f\n", area);
 }

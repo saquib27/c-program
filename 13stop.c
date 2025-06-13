d2b556e7df4e2b8da017be5fa635614e6e68c3df
saquib27 <saquibshamshi27@gmail.com>
2025-06-13 14:57:17 +0530

    13

---
+++ 13stop.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+void main(){
+
+    int x;
+    do{
+        printf("enter your number\n");
+        scanf("%d",&x);
+        printf("%d\n",x);
+        
+        if (x%13==0){
+        break;
+        }
+        }
+while(1); printf("your number is divisible by 13\n");
+}

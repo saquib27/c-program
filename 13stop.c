e86b20026d0d00b98de049a83a08567fcd0a4847
saquib27 <saquibshamshi27@gmail.com>
2025-06-13 22:43:13 +0530

    stop

--- 13stop.c
+++ 13stop.c
@@ -2,10 +2,17 @@
 void main(){
 
     int x;
+
+
     do{
         printf("enter your number\n");
         scanf("%d",&x);
         printf("%d\n",x);
+
+        if(x==0)
+        {
+            continue;
+        }
         
         if (x%13==0){
         break;

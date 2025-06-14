35eb97d4087b4823884b07118c1322865e5b8789
saquib27 <saquibshamshi27@gmail.com>
2025-06-14 15:10:59 +0530

    net price

--- gst.c
+++ gst.c
@@ -2,13 +2,14 @@
 void main(){
 float price[3];
 printf("enter three price:");
-scanf("%f",&price[0]);
-scanf("%f",&price[1]);
-scanf("%f",&price[2]);
 
-printf("total price of 1st; %f\n",price[0]+(0.18*price[0]));
-printf("total price of 2nd; %f\n",price[1]+(0.18*price[1]));
-printf("total price of 3rd; %f\n",price[2]+(0.18*price[2]));
+for(int i=0;i<=2;i++)
+{
+    scanf("%f",&price[i]);
+}
+printf("total price of 1st item: %f\n",price[0]+(0.18*price[0]));
+printf("total price of 2nd item: %f\n",price[1]+(0.18*price[1]));
+printf("total price of 3rd item: %f\n",price[2]+(0.18*price[2]));
 
 
 

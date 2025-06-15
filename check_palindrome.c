a725adab5a46a9fd3f75d547a5595ca015755ad2
saquib27 <saquibshamshi27@gmail.com>
2025-06-15 16:54:36 +0530

    check whether input is palindrome or not

---
+++ check_palindrome.c
@@ -0,0 +1,38 @@
+#include<stdio.h>
+void main(){
+int total_number_of_element;
+    printf("enter total number of number\n");
+    scanf("%d", &total_number_of_element);
+    int number[total_number_of_element];
+    int is_equal=1;
+    printf("enter your numbers \n");
+    for (int i = 0; i < total_number_of_element; i++)
+    {
+        scanf("%d", &number[i]);
+    }
+    for(int i=0;i<total_number_of_element/2;i++)
+    {
+        int first_value=number[i];
+        int last_value=number[total_number_of_element-i-1];
+
+        if(first_value!=last_value)
+        {
+            is_equal=0;
+            break;
+        }
+    }
+        
+        if(is_equal==1)
+        {
+            printf("array is a palindrome\n");
+
+        }
+        else{
+            printf("not a palindrome\n");
+        }
+
+
+        
+
+    }
+        

4d8528dfa4fb02843ac1cbfc2dad1055d95ac9ff
saquib27 <saquibshamshi27@gmail.com>
2025-06-15 19:07:51 +0530

    pair sum

---
+++ pair_sum.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+void main()
+{
+    int total_number_of_element;
+    printf("enter total number of number\n");
+    scanf("%d", &total_number_of_element);
+    int number[total_number_of_element];
+    printf("enter your numbers \n");
+    for (int i = 0; i < total_number_of_element; i++)
+    {
+        scanf("%d", &number[i]);
+    }
+    int sum;
+    printf("sum should be \n");
+    scanf("%d", &sum);
+    for (int i = 0; i < total_number_of_element; i++)
+    {
+        for (int j = i + 1; j < total_number_of_element; j++)
+        {
+            if (number[i] + number[j] == sum)
+
+            {
+                printf("pair %d %d \n", number[i], number[j]);
+                
+            }
+            
+            
+        }
+    
+    }
+}

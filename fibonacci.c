6a7a6c048e286396b8402af255a67459dac526cd
saquib27 <saquibshamshi27@gmail.com>
2025-06-17 13:26:14 +0530

    fibonacci.c

--- fibonacci.c
+++ fibonacci.c
@@ -1,32 +1,28 @@
-#include<stdio.h>
-void main(){
-int total_number;
-printf("enter no of term\n");
-scanf("%d",&total_number);
-
-
-if(total_number < 2)
+#include <stdio.h>
+void main()
 {
-    printf("enter no of term >1\n");
+    int total_number;
+    printf("enter no of term\n");
+    scanf("%d", &total_number);
+
+    if (total_number < 2)
+    {
+        printf("enter no of term >1\n");
+    }
+
+    else
+    {
+
+        int series[total_number];
+        series[0] = 0;
+        series[1] = 1;
+        printf("fibonacci series:\t %d %d", series[0], series[1]);
+
+        for (int i = 2; i < total_number; i++)
+        {
+            series[i] = series[i - 1] + series[i - 2];
+            printf(" %d", series[i]);
+        }
+        printf("\n");
+    }
 }
-
-
-else{
-
-
-int series[total_number];
-series[0]=0;
-series[1]=1;
-printf("fibonacci series:\t %d %d",series[0],series[1]);
-
-for(int i=2;i< total_number;i++) {
-  series[i]=series[i-1]+series[i-2];
-  printf(" %d",series[i]);
-}
-printf("\n");
-
-}
-}
-
-
-

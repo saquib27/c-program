03badc6acb14286390f5456f6e7d3c7ac22515c7
saquib27 <saquibshamshi27@gmail.com>
2025-06-18 23:55:05 +0530

    reverse your array from  anywhere

---
+++ reverse_condition.c
@@ -0,0 +1,30 @@
+#include<stdio.h>
+int main(){
+    int total_number_of_element;
+    printf("enter total number of number\n");
+    scanf("%d", &total_number_of_element);
+    int number[total_number_of_element];
+    printf("enter your numbers \n");
+    for (int i = 0; i < total_number_of_element; i++)
+    {
+        scanf("%d", &number[i]);
+    }
+    int x,y;
+    printf("from");
+    scanf("%d",&x);
+    printf("to");
+    scanf("%d",&y);
+     for  (int i=x,j=y;i<=j;i++,j--)
+     {
+        int swap=number[i];
+        number[i]=number[j];
+        number[j]=swap;
+     }
+        printf("array in reverse\n");
+    for(int i=0;i<=(total_number_of_element-1);i++)
+    {
+        printf("%d \n",number[i]);
+    }
+   return 0; 
+}
+

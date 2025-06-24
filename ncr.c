c87b6bd16403ebf1ea40eb74a4bdfab5d5e16fe8
saquib27 <saquibshamshi27@gmail.com>
2025-06-25 00:42:38 +0530

    find nCr

--- ncr.c
+++ ncr.c
@@ -21,13 +21,14 @@ int main(){
     if (n < r || n < 0 || r < 0) {
         printf("invalid input \n");
     }
-
+    else{
     long double nfactorial=factorial(n);
     long double rfactorial=factorial(r);
     long double nrfactorail=factorial(n-r);
 
     long double ncr=nfactorial/(rfactorial*nrfactorail);
     printf("nCr = %.0Lf \n",ncr);
+    }
 
 }
 

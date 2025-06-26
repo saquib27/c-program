c40db97c0f15d705bc275b0abe6e97bc7809c11f
saquib27 <saquibshamshi27@gmail.com>
2025-06-26 20:05:12 +0530

    pascal triangle

--- pascal_triangle.c
+++ pascal_triangle.c
@@ -24,7 +24,7 @@ int main(){
     int n;
     printf("enter n\t");
     scanf("%d",&n);
-    for(int i=0;i<=n;i++){
+    for(int i=0;i<=n-1;i++){
         for(int j=0;j<=i;j++){
             int icj= ncr(i,j);
             printf("%d ",icj);

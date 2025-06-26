5b93745abba925951acab96982fa558b4d990c0b
saquib27 <saquibshamshi27@gmail.com>
2025-06-26 13:49:10 +0530

    pyramid pattern

---
+++ pyramid.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+int main()
+{
+    int lines;
+    printf("enter number of line \t");
+    scanf("%d", &lines);
+    for (int i = 0; i <= lines+2; i++)
+    {
+        for (int j = 0; j <=  i ; j++)
+        {
+            if (j >= lines + (-i + 1) && j <= lines + (i - 1))
+            {
+                printf("*\t");
+            }
+            else
+                printf(" \t");
+
+        }
+        printf("\n");
+    }
+}

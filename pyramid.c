8c85b246cfeec83dc57262a44030b1d513097cc4
saquib27 <saquibshamshi27@gmail.com>
2025-06-27 20:48:02 +0530

    pyramid with *

--- pyramid.c
+++ pyramid.c
@@ -4,9 +4,9 @@ int main()
     int lines;
     printf("enter number of line \t");
     scanf("%d", &lines);
-    for (int i = 0; i <= lines+2; i++)
+    for (int i = 0; i <= lines + 2; i++)
     {
-        for (int j = 0; j <=  i ; j++)
+        for (int j = 0; j <= i; j++)
         {
             if (j >= lines + (-i + 1) && j <= lines + (i - 1))
             {
@@ -14,7 +14,6 @@ int main()
             }
             else
                 printf(" \t");
-
         }
         printf("\n");
     }

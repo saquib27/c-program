4020bad73f2f82342446006aeda5134e7a575f1c
saquib27 <saquibshamshi27@gmail.com>
2025-07-03 20:10:14 +0530

    sort your array

--- sorting.c
+++ sorting.c
@@ -1,5 +1,6 @@
-#include<stdio.h>
-int main(){
+#include <stdio.h>
+int main()
+{
     int total_number_of_element;
     printf("enter total number of element\n");
     scanf("%d", &total_number_of_element);
@@ -10,29 +11,28 @@ int main(){
         scanf("%d", &number[i]);
     }
     int max;
-   
-    for(int i=0;i<total_number_of_element-1;i++){
 
-    for(int j=i+1;j<total_number_of_element;j++){
-        if(number[i]>=number[j]){
-            max=number[i];
-        
-    
-            int swap=number[i];
-            number[i]=number[j];
-            number[j]=swap;
+    for (int i = 0; i < total_number_of_element - 1; i++)
+    {
 
+        for (int j = i + 1; j < total_number_of_element; j++)
+        {
+            if (number[i] >= number[j])
+            {
+                max = number[i];
+
+                int swap = number[i];
+                number[i] = number[j];
+                number[j] = swap;
+            }
+        }
     }
-}
-    }
-    
+
     printf("sorting \n");
-    for(int i=0;i<total_number_of_element;i++){
-        printf("%d \n",number[i]);
+    for (int i = 0; i < total_number_of_element; i++)
+    {
+        printf("%d \n", number[i]);
     }
 
-
-return 0;
-    }
-
-
+    return 0;
+}

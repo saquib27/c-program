f96d34e34d1f55ff2d5b8b41ed3f4afbb486bb59
saquib27 <saquibshamshi27@gmail.com>
2025-07-04 22:22:22 +0530

    zero at end

--- zeroend.c
+++ zeroend.c
@@ -1,33 +1,40 @@
-#include<stdio.h>
-int main(){
+#include <stdio.h>
+int main()
+{
     int number_of_element;
     printf("enter number of element\t");
-    scanf("%d",&number_of_element);
+    scanf("%d", &number_of_element);
     if (number_of_element <= 0)
     {
         printf("enter valid value \n");
     }
-    else{
+    else
+    {
         int number[number_of_element];
         printf("enter your number\n");
-        for(int i=0;i<number_of_element;i++){
-            scanf("%d",&number[i]);
+        for (int i = 0; i < number_of_element; i++)
+        {
+            scanf("%d", &number[i]);
         }
-    
-    int zero=0;
-    for(int i=0;i<number_of_element;i++){
-        if(number[i]!=0){
-            number[zero]=number[i];
+
+        int zero = 0;
+        for (int i = 0; i < number_of_element; i++)
+        {
+            if (number[i] != 0)
+            {
+                number[zero] = number[i];
+                zero++;
+            }
+        }
+        while (zero < number_of_element)
+        {
+            number[zero] = 0;
             zero++;
         }
-    }
-    while(zero<number_of_element){
-        number[zero]=0;
-        zero++;
-    }
-     printf("zero at end\n");
-     for(int i=0;i<number_of_element;i++){
-        printf("%d\n",number[i]);
-     }
+        printf("zero at end\n");
+        for (int i = 0; i < number_of_element; i++)
+        {
+            printf("%d\n", number[i]);
+        }
     }
 }

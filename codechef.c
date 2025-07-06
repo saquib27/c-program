0f4585912cdec82e70da6ca3908eca827a4232b0
saquib27 <saquibshamshi27@gmail.com>
2025-07-06 14:40:08 +0530

    codechef

--- codechef.c
+++ codechef.c
@@ -1,3 +1,44 @@
+ /*Find maximum in an Array
+Given a list of 
+N
+N integers, representing height of mountains. Find the height of the tallest mountain.
+
+Input:
+First line will contain 
+T
+T, number of testcases. Then the testcases follow.
+The first line in each testcase contains one integer, 
+N
+N.
+The following line contains 
+N
+N space separated integers: the height of each mountains.
+Output:
+For each testcase, output one line with one integer: the height of the tallest mountain for that test case.
+
+Constraints
+1
+≤
+T
+≤
+10
+1≤T≤10
+1
+≤
+N
+≤
+100000
+1≤N≤100000
+0
+≤
+0≤ height of each mountain 
+≤
+10
+9
+≤10 
+9*/
+ 
+ 
  #include <stdio.h>
 
 int main() {

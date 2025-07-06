f4bd10bc2cf35d30067ecd03871e519022f8323a
saquib27 <saquibshamshi27@gmail.com>
2025-07-06 20:39:13 +0530

    detect 42

---
+++ spoj.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+int main(){
+
+    int x;
+
+
+    do{
+        scanf("%d",&x);
+
+        if(x==42)
+    {
+      break;
+        
+    printf("%d\n",x);
+    }
+   
+}
+    while (1);  
+
+    return 0;
+        }
+        
+

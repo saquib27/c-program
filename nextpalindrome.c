dc3e4826604b8f85031485feb6fd01f011441ab4
saquib27 <saquibshamshi27@gmail.com>
2025-07-15 19:46:01 +0530

    next smallest palindrome

--- nextpalindrome.c
+++ nextpalindrome.c
@@ -1,7 +1,21 @@
+/*A positive integer is called a palindrome if its representation in the decimal system is the same when read from 
+left to right and from right to left. For a given positive integer K of not more than 1000000 digits, write the value
+ of the smallest palindrome larger than K to output. Numbers are always displayed without leading zeros.
+
+Input
+The first line contains integer t, the number of test cases. Integers K are given in the next t lines.
+
+Output
+For each K, output the smallest palindrome larger than K.*/
+
+
+
+
+
 #include<stdio.h>
   int palindrome(int a){
-    int input= a;
-    int check=0;
+    long double input= a;
+    long double check=0;
     while (a > 0) {
         check = check * 10 + (a % 10);
         a = a / 10;
@@ -14,15 +28,15 @@ int main() {
 
     while (t--){
 
-        int number;
-        scanf("%d",&number);
+        long double number;
+        scanf("%Lf",&number);
         number=number+1;
         while (!palindrome(number)) {
             number=number+1;
       
 
         }
-        printf("%d\n",number);
+        printf("%Lf\n",number);
     }
     return 0;
 }

d2db5fb54be46371c5af43f4bcac27778e91b008
saquib27 <saquibshamshi27@gmail.com>
2025-07-16 19:40:03 +0530

    size of string

--- strsize.c
+++ strsize.c
@@ -1,17 +1,18 @@
-#include<stdio.h>
-#include<string.h>
-int main(){
+#include <stdio.h>
+#include <string.h>
+int main()
+{
     char str[1000];
     puts("enter your input");
-    scanf("%[^\n]s",str);
+    scanf("%[^\n]s", str);
 
-    int size=0;
-    int i=0;
-    while(str[i]!='\0'){
-        size=size+1;
+    int size = 0;
+    int i = 0;
+    while (str[i] != '\0')
+    {
+        size = size + 1;
         i++;
     }
-    printf("size of input is  %d",size);
+    printf("size of input is  %d", size);
     printf("\n");
-
 }

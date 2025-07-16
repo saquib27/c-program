631669618a1bcd1b953ce6f9a382a5fb37b20c08
saquib27 <saquibshamshi27@gmail.com>
2025-07-16 19:38:25 +0530

    reverse your string

--- strreverse.c
+++ strreverse.c
@@ -1,22 +1,24 @@
-#include<stdio.h>
-#include<string.h>
-int main(){
+#include <stdio.h>
+#include <string.h>
+int main()
+{
     char str[1000];
     puts("enter your input");
-    scanf("%[^\n]s",str);
+    scanf("%[^\n]s", str);
 
-    int size=0;
-    int k=0;
-    while(str[k]!='\0'){
-        size=size+1;
+    int size = 0;
+    int k = 0;
+    while (str[k] != '\0')
+    {
+        size = size + 1;
         k++;
     }
-    for(int i=0,j=size-1;i<=j;i++,j--){
-        char swap=str[i];
-        str[i]=str[j];
-        str[j]=swap;
+    for (int i = 0, j = size - 1; i <= j; i++, j--)
+    {
+        char swap = str[i];
+        str[i] = str[j];
+        str[j] = swap;
     }
-    printf("string in reverse \n%s",str);
+    printf("string in reverse \n%s", str);
     printf("\n");
-
 }

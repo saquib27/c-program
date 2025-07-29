624023cba991efb1df87d72a59859f55d0ed9c34
saquib27 <saquibshamshi27@gmail.com>
2025-07-29 19:22:27 +0530

    No of ways to climb stair max 3 at a time

---
+++ climbingstair3.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+int steps(int stair)
+{
+    if (stair == 1)
+    {
+        return 1;
+    }
+    if (stair == 2)
+    {
+        return 2;
+    }
+    if(stair==3){
+        return 4;
+    }
+    int no_of_ways = steps(stair - 1) + steps(stair - 2) + steps(stair-3);
+    return no_of_ways;
+}
+
+int main()
+{
+    int stair;
+    printf("enter no of stair\n");
+    scanf("%d", &stair);
+    if(stair<=0){
+        printf("enter valid no of stairs\n");
+    }
+    else{
+    int ways = steps(stair);
+    printf("no of ways  %d\n", ways);
+    }
+    return 0;
+}

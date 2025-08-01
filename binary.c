44b7e677f403a9e1a68f79bb7b530539cc9fcb78
saquib27 <saquibshamshi27@gmail.com>
2025-08-02 02:33:23 +0530

    binary number

---
+++ binary.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+void binary(int x){
+     if (x == 1) {
+        printf("1");
+        return;
+     }
+     binary(x/2);
+     printf("%d",x%2);
+}
+int main(){
+    int number;
+    printf("enter your number\n");
+    scanf("%d",&number);
+    
+    if (number==0){
+        printf("0\n");
+    }
+    else {
+        printf("binary: ");
+        binary(number);
+        printf("\n");
+    }
+}

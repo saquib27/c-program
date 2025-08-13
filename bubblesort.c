ff306f628b657f22b814cf5e4b610fae6827c734
saquib27 <saquibshamshi27@gmail.com>
2025-08-13 15:48:23 +0530

    bubble sorting

--- bubblesort.c
+++ bubblesort.c
@@ -12,7 +12,7 @@ int main()
     }
 
     for(int i=0;i<=total_number_of_element-1;i++){
-        for(int j=0;j<=total_number_of_element-2;j++){
+        for(int j=0;j<total_number_of_element-1-i;j++){
             if(number[j]>number[j+1]){
                 int temp=number[j];
                 number[j]=number[j+1];

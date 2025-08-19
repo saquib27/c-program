eb8a57d647f4a04e3856f73efe4320f66a9f3de8
saquib27 <saquibshamshi27@gmail.com>
2025-08-19 23:43:23 +0530

    prime

--- prime_generator.c
+++ prime_generator.c
@@ -1,12 +1,11 @@
 /*Peter wants to generate some prime numbers for his cryptosystem. Help him! Your task is to generate all prime numbers between two given numbers!
 
 Input
-The input begins with the number t of test cases in a single line (t ≤ 10). In each of the next t lines there are two numbers m and n (1 ≤ m ≤ n ≤ 1000000000, n-m ≤ 100000) separated by a space.
+The input begins with the number t of test cases in a single line (t ≤ 10). In each of the next t lines there are two numbers m and n
+(1 ≤ m ≤ n ≤ 1000000000, n-m ≤ 100000) separated by a space.
 
 Output
 For every test case print all prime numbers p such that m ≤ p ≤ n, one number per line, test cases separated by an empty line.*/
-
-
 
 #include <stdio.h>
 #include <math.h>
@@ -28,7 +27,7 @@ int main()
             {
 
                 int prime = 0;
-            
+
                 for (int j = 2; j <= sqrt(i); j++)
                 {
                     if (i % j == 0)

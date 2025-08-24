7ec389bcc18ab93bbdd1765bc0821349436e0f6e
saquib27 <saquibshamshi27@gmail.com>
2025-08-24 21:06:07 +0530

    spoj

--- factorial.c
+++ factorial.c
@@ -17,25 +17,66 @@
 // else 
 //     printf("invalid number\n");
 // }
+// #include <stdio.h>
+// int main()
+// {
+//     int t;
+//     scanf("%d", &t);
+
+//     while (t--){
+//     int x;
+//     scanf("%d", &x);
+
+//  long double factorial=1;
+//     if(x>=0){
+
+//     for (int i = 1; i <= x; i++)
+//     {
+//         factorial = factorial * i;
+//     }
+//     printf("%.0Lf\n", factorial);
+// }
+// }
+// return 0;
+// }
+
 #include <stdio.h>
-int main()
-{
+
+#define MAX 200 
+
+int main() {
     int t;
     scanf("%d", &t);
 
-    while (t--){
-    int x;
-    scanf("%d", &x);
+    while (t--) {
+        int x;
+        scanf("%d", &x);
 
- long double factorial=1;
-    if(x>=0){
+        
+        int res[MAX];
+        int res_size = 1;
+        res[0] = 1; 
 
-    for (int i = 1; i <= x; i++)
-    {
-        factorial = factorial * i;
+        if (x >= 0) {
+            for (int i = 2; i <= x; i++) {
+                int carry = 0;
+                for (int j = 0; j < res_size; j++) {
+                    int prod = res[j] * i + carry;
+                    res[j] = prod % 10;
+                    carry = prod / 10;
+                }
+                while (carry) {
+                    res[res_size++] = carry % 10;
+                    carry /= 10;
+                }
+            }
+
+        
+            for (int i = res_size - 1; i >= 0; i--)
+                printf("%d", res[i]);
+            printf("\n");
+        }
     }
-    printf("%.0Lf\n", factorial);
+    return 0;
 }
-}
-return 0;
-}
+

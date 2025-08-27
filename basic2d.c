f31c1f34e39c6e49191559cf1748015003600e83
saquib27 <saquibshamshi27@gmail.com>
2025-08-27 17:25:53 +0530

    basic2d

--- basic2d.c
+++ basic2d.c
@@ -1,27 +1,30 @@
-#include<stdio.h>
-int main(){
-    int row,colume;
+#include <stdio.h>
+int main()
+{
+    int row, colume;
     printf("enter number of rows: \t");
-    scanf("%d",&row);
+    scanf("%d", &row);
     printf("enter number of colume: \t");
-    scanf("%d",&colume);
-    int array [row][colume];
-    
-    for(int i=0;i<row;i++){
-        for(int j=0;j<colume;j++){
+    scanf("%d", &colume);
+    int array[row][colume];
+
+    for (int i = 0; i < row; i++)
+    {
+        for (int j = 0; j < colume; j++)
+        {
             printf("enter your number");
-            scanf("%d",&array[i][j]);
+            scanf("%d", &array[i][j]);
         }
     }
 
-    for (int i=0;i<row;i++){
-        for(int j=0;j<colume;j++){
-            printf("%d",array[i][j]);
+    for (int i = 0; i < row; i++)
+    {
+        for (int j = 0; j < colume; j++)
+        {
+            printf("%d", array[i][j]);
         }
-         printf("\n");
+        printf("\n");
     }
-    
 
-   
-return 0;
+    return 0;
 }

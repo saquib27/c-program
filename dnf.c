6cbf9395f037ab577fdc1cc8ea287233f55fdb2d
saquib27 <saquibshamshi27@gmail.com>
2025-08-30 20:17:56 +0530

    ~Dutch National Flag Problem

--- dnf.c
+++ dnf.c
@@ -1,9 +1,11 @@
 #include <stdio.h>
 
-void sort012(int arr[], int n) {
+void sort012(int arr[], int n)
+{
     int c0 = 0, c1 = 0, c2 = 0;
 
-    for (int i = 0; i < n; i++) {
+    for (int i = 0; i < n; i++)
+    {
         if (arr[i] == 0)
             c0 += 1;
         else if (arr[i] == 1)
@@ -13,12 +15,10 @@ void sort012(int arr[], int n) {
     }
 
     int idx = 0;
-    
-    
+
     for (int i = 0; i < c0; i++)
         arr[idx++] = 0;
 
-    
     for (int i = 0; i < c1; i++)
         arr[idx++] = 1;
 
@@ -26,8 +26,9 @@ void sort012(int arr[], int n) {
         arr[idx++] = 2;
 }
 
-int main() {
-    int arr[] = { 0, 1, 2, 0, 1, 2 };
+int main()
+{
+    int arr[] = {0, 1, 2, 0, 1, 2};
     int n = sizeof(arr) / sizeof(arr[0]);
 
     sort012(arr, n);
